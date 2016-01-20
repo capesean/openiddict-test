a3ba1e335700bd8f4473ed7186aa17619d5b6b6e
Sean Walsh <seanmatthewwalsh@gmail.com>
2016-01-21 00:15:49 +0200

    api & test api

---
+++ src/openiddict-test/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using openiddicttest.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+[Authorize]
+public class TestController : Controller
+{
+    private ApplicationDbContext _context;
+    private UserManager<ApplicationUser> _userManager;
+
+    public TestController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+    [Route("api/test"), HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var user = await _userManager.FindByIdAsync(User.GetUserId());
+        if (user == null) return Ok("No user / not logged in");// if Authorize is not applied
+        return Ok(user);
+    }
+}
